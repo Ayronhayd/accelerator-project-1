e6b7b2f2738927278320be08c6603b97e43bec1a
Игорь Щербаков <dj_jazz2010@mail.ru>
2024-07-06 15:53:22 +0300

    about

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,16 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+
+const video = document.querySelector('.video');
+const button = document.querySelector('.video__play');
+const src = video.dataset.src;
+
+button.addEventListener('click', () => {
+  if (video.classList.contains('video__iframe')) {
+    return;
+  }
+  video.classList.add('video__iframe');
+  video.insertAdjacentHTML('afterbegin', `<iframe class="video__iframe" src="${src}" title="video" frameborder="1" allow="autoplay"; allowfullscreen></iframe>`);
+});
