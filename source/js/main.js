c063ba3420b9db692e7c60f3b42b17ea7a6149d3
Игорь Щербаков <dj_jazz2010@mail.ru>
2024-07-13 17:36:43 +0300

    juri

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,6 @@
-// https://swiperjs.com/get-started#installation
-
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation } from 'swiper/modules';
 import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
 
 const video = document.querySelector('.video');
 const button = document.querySelector('.video__play');
@@ -19,31 +14,30 @@ button.addEventListener('click', () => {
 });
 
 
-const swiper = new Swiper('.swiper', {
-  modules: [Navigation, Pagination],
+const swiper = new Swiper('.swiper__container', {
+  modules: [Navigation],
   loop: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  allowTouchMove: true,
   breakpoints: {
     320: {
-      slidesPerView: 1
+      slidesPerView: 1,
+      spaceBetween: 40,
     },
     768: {
       slidesPerView: 2,
-      allowTouchMove: true
+      allowTouchMove: true,
+      spaceBetween: 40,
     },
     1366: {
-      slidesPerView: 4
+      slidesPerView: 4,
+      spaceBetween: 40,
     }
-  },
-  grid: {
-    rows: 2,
-    fill: 'column',
   },
 });
 
 
-swiper.slideNext();
+swiper();
+
