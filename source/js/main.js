1844b31c135d184db131368195c2b5ea56431a98
Игорь Щербаков <dj_jazz2010@mail.ru>
2024-07-21 18:23:00 +0300

    form

--- source/js/main.js
+++ source/js/main.js
@@ -51,3 +51,26 @@ const swiper1 = new Swiper('.reviews__swiper', {
 });
 
 swiper1.update();
+
+const form = document.querySelector('.form__form');
+const nameInput = form.querySelector('.form__input--name');
+const nameError = form.querySelector('.form__error-name');
+const phoneInput = form.querySelector('.form__input--phone');
+const phoneError = form.querySelector('.form__error-phone');
+const NAME_VALID = /^[a-zA-Zа-яА-ЯЁё]+$/;
+const PHONE_VALID = /\+\d/;
+
+
+const validateInput = (input, regex, error) => {
+  const isValid = regex.test(input.value);
+  error.style.display = isValid ? 'none' : 'block';
+  input.classList.toggle('form__input--error', !isValid);
+  return isValid;
+};
+form.addEventListener('submit', (e) => {
+  const isNameValid = validateInput(nameInput, NAME_VALID, nameError);
+  const isPhoneValid = phoneInput.value ? validateInput(phoneInput, PHONE_VALID, phoneError) : true;
+  if (!isNameValid || !isPhoneValid) {
+    e.preventDefault();
+  }
+});
